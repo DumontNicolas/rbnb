4c652d68ed65ce695f05de1c4903ca785cbe598f
Nicolas Dumont <nicolas.dumont@essca.eu>
2021-02-15 15:49:31 +0100

    Pushed for collaborators

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games
+  resources :reservations, only: [:index, :new, :create, :update, :edit] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :users, only: :show do
+    resources  :reviews, only: :index
+  end
 end
