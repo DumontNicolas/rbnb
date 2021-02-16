f3f79474dfcc29818d1aadb44716a2ebdfbf58a0
Pierre Bodin <pro@pierrebodin.com>
2021-02-16 10:26:14 +0100

    added dependences to model user and game

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :games
-  has_many :reviews
-  has_many :reservations
+  has_many :games, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   validates :first_name, :last_name, :email, :location, presence: true
   # Include default devise modules. Others available are:
