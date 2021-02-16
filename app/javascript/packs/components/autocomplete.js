5a901c41404349505fcc6a8f70961e9e3cfdd715
Nicolas Dumont <nicolas.dumont@essca.eu>
2021-02-16 13:46:16 +0100

    Changed js URL

--- app/javascript/packs/components/autocomplete.js
+++ app/javascript/packs/components/autocomplete.js
@@ -4,14 +4,14 @@ const search = document.querySelector(".form-control");
 const autoComplete = () => {
   search.addEventListener("keyup", (event) => {
     // console.log(event.target.value);
-    fetch(`http://localhost:3000/search?name=${event.target.value}`)
+    fetch(`http://${window.location.hostname}/search?name=${event.target.value}`)
     .then(response => response.json())
     .then((data) => {
       // console.log(data);
       results.innerHTML = "";
       data.forEach(element => {
         // console.log(element.name);
-        results.insertAdjacentHTML('beforeend', `<li><a href="http://localhost:3000/games/${element.id}">${element.name}</a></li>`)
+        results.insertAdjacentHTML('beforeend', `<li><a href="http://${window.location.hotsname}/games/${element.id}">${element.name}</a></li>`)
       });
     });
   });
