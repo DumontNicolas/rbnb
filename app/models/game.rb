9f3457cea18838ab005bce301456bcd3a7ad9006
jay cormier <jay.cormier@me.com>
2021-02-17 10:34:17 +0000

    added Cloudinary support

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, :description, :category, :price, presence: true
 end
